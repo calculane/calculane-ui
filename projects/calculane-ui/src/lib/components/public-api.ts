a2ce05880631ac9d83da5b1c068ef0b6c89a7e07
albertkombol <kombolalbert@gmail.com>
2021-12-06 23:35:11 +0100

    create card component

--- projects/calculane-ui/src/lib/components/public-api.ts
+++ projects/calculane-ui/src/lib/components/public-api.ts
@@ -1,6 +1,15 @@
+//Button
 export * from './button/button.module';
 export * from './button/button.component';
 export * from './button/button-shape.enum';
 export * from './button/button-size.enum';
 export * from './button/button-style.enum';
 export * from './button/button-color.enum';
+
+//Card
+export * from './card/card.module';
+export * from './card/card.component';
+export * from './card/card-body.component';
+export * from './card/card-footer.component';
+export * from './card/card-header.component';
+export * from './card/card-type.enum';
